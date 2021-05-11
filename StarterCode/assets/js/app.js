b705c9abbd91786e2cfef1771cc1fec45ba357c7
John Glen <jglendening11@gmail.com>
2021-05-10 22:18:22 -0500

    add

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -67,3 +67,21 @@ function renderYAxes(newYScale, yAxis) {
 
     return yAxis;
 }
+// functions to update circles 
+function renderXCircles(circlesGroup, newXScale, chosenXAxis) {
+
+    circlesGroup.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[chosenXAxis]));
+
+    return circlesGroup;
+}
+
+function renderYCircles(circlesGroup, newYScale, chosenYAxis) {
+
+    circlesGroup.transition()
+        .duration(1000)
+        .attr("cy", d => newYScale(d[chosenYAxis]));
+
+    return circlesGroup;
+}
